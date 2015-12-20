ab8c090f2c5adae571aecf46d3a319734b4ba996
vadim <mr.rjkov96@gmail.com>
2015-12-20 16:14:29 +0200

    Created project

---
+++ src/QuestFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+/**
+ * Created by vadim on 20.12.15.
+ */
+public class QuestFrame {
+    private JTextArea textArea1;
+    private JPanel panel1;
+    private JList list1;
+
+
+
+}
